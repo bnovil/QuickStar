3d3d5eff325192a7a161a5ed87d13ae95168e959
bnovil <renovil@163.com>
2017-05-27 20:34:27 +0800

    user

---
+++ src/main/java/com/quickstar/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.quickstar.controller;
+
+/**
+ * Created by lzq on 2017/5/27.
+ *
+ * @Description
+ */
+public class UserController {
+}
---
+++ src/main/java/com/quickstar/service/interfaces/UserService.java
@@ -0,0 +1,9 @@
+package com.quickstar.service.interfaces;
+
+/**
+ * Created by lzq on 2017/5/27.
+ *
+ * @Description
+ */
+public interface UserService {
+}
