18fef4fc07965919d8a6aaba70eacbc9a3c89e07
bnovil <renovil@163.com>
2017-09-07 21:18:08 +0800

    delete redundant code

--- src/main/java/com/quickstar/thread/deviceio/DeviceServerHandler.java
+++ src/main/java/com/quickstar/thread/deviceio/DeviceServerHandler.java
@@ -25,32 +25,9 @@ public class DeviceServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws IOException {
         ByteBuf in = (ByteBuf) msg;
-        ByteBuf tofile = (ByteBuf) msg;
         String str = in.toString(CharsetUtil.UTF_8);
         log.info(str);
-        String[] data = str.split("\\*");
-        String subdata = data[3].substring(0,2);
-        if ("LK".equals(subdata)) {
-            String toclient = data[0]+"*"+data[1]+"*"+"0002"+"*"+"LK]";
-            log.info("send to client: "+toclient);
-            ByteBuf b = Unpooled.copiedBuffer(toclient,CharsetUtil.UTF_8);
-            ctx.writeAndFlush(b);
-
-        }
-        if ("UP".equals(subdata)) {
-            log.info("change time internal succeed");
-        }
-        if ("TK".equals(subdata)) {
-            FileOutputStream fos = new FileOutputStream(new File("C:\\watchtest\\w"+data[2]+".amr"));
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-            byte[] tf = new byte[tofile.readableBytes()];
-            tofile.readBytes(tf);
-            int start = data[0].length()+1+data[1].length()+1+data[2].length()+4;
-            bos.write(tf, start, tf.length - start -1);
-            bos.flush();
-        }
-        ByteBuf timeinternal = Unpooled.copiedBuffer((data[0]+"*"+data[1]+"*"+data[2]+"*"+"UPLOAD,1]"),CharsetUtil.UTF_8);
-        ctx.writeAndFlush(timeinternal);
+        ctx.writeAndFlush("server received ");
     }
 
     //连接建立
